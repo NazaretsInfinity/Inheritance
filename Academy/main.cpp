78238767a2a12d8cf951b2e6fc566622d6fbf1aa
Nazarets <ksenianazarec995@mail.com>
2024-06-26 19:54:46 +0300

    fixed cout(no unnecessary nlines

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -11,56 +11,57 @@ class Human
 	unsigned int age;
 public:
 	// INCAPSULATION
-const std::string& get_last_name()const
+	const std::string& get_last_name()const
 	{
-	return last_name;
+		return last_name;
 	}
-const std::string& get_first_name()const
-{
-	return first_name;
-}
-const unsigned int get_age()const
-{
-	return age;
-}
-void set_last_name(std::string lstn)
-{
-	last_name = lstn;
-}
-void set_first_name(std::string frst)
-{
-	first_name = frst;
-}
-void set_age(unsigned int age)
-{
-	this->age = age;
-}
-       //CONSTRUCTORS
-Human(human_take_parameters)
-{
-	set_last_name(last_name);
-	set_first_name(first_name);
-	set_age(age);
-	cout << "HConstructor:\t" << this << endl;
-}
-virtual ~Human()
-{
-	cout << "HUMA_Destructor:\t" << this << endl;
-}
-      //METHODS
-virtual void info()const
-{
-	cout << last_name << " " << first_name << " " << age << " y/o" << endl;
-}
+	const std::string& get_first_name()const
+	{
+		return first_name;
+	}
+	const unsigned int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(std::string lstn)
+	{
+		last_name = lstn;
+	}
+	void set_first_name(std::string frst)
+	{
+		first_name = frst;
+	}
+	void set_age(unsigned int age)
+	{
+		this->age = age;
+	}
+	//CONSTRUCTORS
+	Human(human_take_parameters)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "HConstructor:\t" << this << endl;
+	}
+	virtual ~Human()
+	{
+		cout << "HUMA_Destructor:\t" << this << endl;
+	}
+	//METHODS
+	virtual std::ostream& info(std::ostream& os)const
+	{
+		return  os << last_name << " " << first_name << " " << age << " y/o" << endl;
+	}
 };
+
 std::ostream& operator<<(std::ostream& os, const Human& pers)
 {
-	os << pers.get_last_name() << " " << pers.get_first_name() << " " << pers.get_age() << " y/o";
-    return os;
+	return pers.info(os);
 }
 
 #define student_take_parameters const std::string& speciality, const std::string& group, double rating,double attendance
 #define student_give_parameters speciality, group, rating, attendance
+
 class Student : public Human
 {
 	std::string speciality;
@@ -80,7 +81,7 @@ public:
 	{
 		return rating;
 	}
-	double get_attendance()const
+	double get_attendance()
 	{
 		return attendance;
 	}
@@ -113,20 +114,11 @@ public:
 		cout << "STUD_Destructor:\t" << this << endl;
 	}
 	//METHODS
-	void info()const override
+	std::ostream& info(std::ostream& os)const override
 	{
-		Human::info();
-		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
+		return Human::info(os) << speciality << " " << group << " " << rating << " " << attendance;
 	}
 };
-std::ostream& operator<<(std::ostream& os, const Student& pers)
-{
-	pers.Human::info();
-	os << pers.get_speciality() << " " << pers.get_group() << " " << pers.get_rating() << " "
-	<< pers.get_attendance();
-	return os;
-}
-
 class Teacher : public Human
 {
 	std::string speciality;
@@ -143,28 +135,27 @@ public:
 	void set_speciality(const std::string speciality)
 	{
 		this->speciality = speciality;
-}
+	}
 	void set_experience(unsigned int experience)
 	{
 		this->experience = experience;
-    }
+	}
 	//Constructors
 	Teacher(human_take_parameters, std::string speciality, unsigned int experience) : Human(human_give_parameters)
 	{
 		set_speciality(speciality);
 		set_experience(experience);
 		cout << "Constructor:\t" << this << endl;
- 	}
+	}
 	//Destructor
 	~Teacher()
 	{
 		cout << "TEAC_Destructor:\t" << this << endl;
 	}
 	//Methods
-	void info()const override
+	std::ostream& info(std::ostream& os)const override
 	{
-		Human::info();
-		cout << speciality << " " << experience << "-years" << endl;
+		return Human::info(os) << speciality << " " << experience << "-years";
 	}
 };
 class Graduate : public Student
@@ -173,41 +164,40 @@ class Graduate : public Student
 	unsigned int workgrade;
 public:
 	// incaps
-const std::string get_topic()const
-{
-	return worktopic;
-}
-const unsigned int get_grade()const
-{
-	return workgrade;
-}
-void set_topic(std::string worktopic)
-{
-	this->worktopic = worktopic;
-}
-void set_grade(unsigned int workgrade)
-{
-	this->workgrade = workgrade;
-}
-    //Constructor
-Graduate(human_take_parameters, student_take_parameters, std::string worktopic, unsigned int workgrade)
-	: Student(human_give_parameters, student_give_parameters)
-{
-	set_topic(worktopic);
-	set_grade(workgrade);
-	cout << "GConstructor:\t" << this << endl;
-}
-   //Destructor
-~Graduate()
-{
-	cout << "GRAD_Destructor:\t" << this << endl;
-}
-  //Methods
-void info()const override
-{
-	Student::info();
-	cout << worktopic << " got: " << workgrade << endl;
-}
+	const std::string get_topic()const
+	{
+		return worktopic;
+	}
+	const unsigned int get_grade()const
+	{
+		return workgrade;
+	}
+	void set_topic(std::string worktopic)
+	{
+		this->worktopic = worktopic;
+	}
+	void set_grade(unsigned int workgrade)
+	{
+		this->workgrade = workgrade;
+	}
+	//Constructor
+	Graduate(human_take_parameters, student_take_parameters, std::string worktopic, unsigned int workgrade)
+		: Student(human_give_parameters, student_give_parameters)
+	{
+		set_topic(worktopic);
+		set_grade(workgrade);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	//Destructor
+	~Graduate()
+	{
+		cout << "GRAD_Destructor:\t" << this << endl;
+	}
+	//Methods
+	std::ostream& info(std::ostream& os)const override
+	{
+		return Student::info(os) << endl << worktopic << " got: " << workgrade;
+	}
 };
 
 #define InheritanceCheck
@@ -233,10 +223,7 @@ void main()
 	for (int i = 0; i < sizeof(group) / sizeof(group[i]); i++)
 	{
 		//group[i]->info(); 
-		cout << typeid(*group[i]).name() << ":" << endl;
-		if ((typeid(*group[i])) == typeid(Student))cout << *dynamic_cast<Student*>(group[i]);
-		else cout << *group[i];
-		cout << delimeter;
+		cout << *group[i] << delimeter;
 	}
 	for (int i = 0; i < sizeof(group) / sizeof(group[i]); i++) //CALL OF DISTRCTR
 	{
